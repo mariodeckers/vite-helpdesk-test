adeef76b799a6e412947eaef7dbfa95c73e22728
Mario Deckers <mariodeckers@gmail.com>
2025-11-14 11:38:27 +0100

    setup: add Shadcn/ui and Inter font

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,4 +18,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
